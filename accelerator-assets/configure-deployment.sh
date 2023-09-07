4b7706ab539d24a71ddb0550d09d21ed2d110b33
fynesy <tfynes@vmware.com>
2023-09-07 17:30:51 -0400

    .

---
+++ accelerator-assets/configure-deployment.sh
@@ -0,0 +1,29 @@
+
+pushd ./secrets-to-seal
+./encrypt-secrets.sh
+popd
+
+
+mv ./secrets-to-seal/config/tanzu-registry-secret.sops.yaml ../clusters/taplab/cluster-config/config/
+mv ./secrets-to-seal/config/user-registry-dockerconfig.sops.yaml ../clusters/taplab/cluster-config/config/
+mv ./secrets-to-seal/config/workload-git-auth.sops.yaml          ../clusters/taplab/cluster-config/config/
+
+mv ./secrets-to-seal/dependent-resources/sso-credentials-secret.sops.yaml ../clusters/taplab/cluster-config/dependent-resources/
+
+mkdir -p ../clusters/taplab/tanzu-sync/app/sensitive-values
+mv ./secrets-to-seal/tanzu-sync/tanzu-sync-values.sops.yaml ../clusters/taplab/tanzu-sync/app/sensitive-values/
+
+# mv ./secrets-to-seal/tanzu-sync/registry-credentials.sops.yaml ../clusters/taplab/tanzu-sync/config/.tanzu-managed/
+
+mv ./secrets-to-seal/values/tap-sensitive-values.sops.yaml ../clusters/taplab/cluster-config/values/
+
+if [ tlsHttpSolver = 'tlsManual' ]
+then
+mv ./secrets-to-seal/tls/taplab-tls-certs.sops.yaml ../clusters/taplab/cluster-config/config/
+fi
+
+mv ./non-sensitive-config/tap-non-sensitive-values.yaml             ../clusters/taplab/cluster-config/values/
+
+echo
+echo DELETE /accelerator-log.md BEFORE PUSHING TO REPO AS IT CONTAINS SENSITIVE VALUES
+echo
